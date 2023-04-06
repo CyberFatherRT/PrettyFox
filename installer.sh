ee36a21c2c5d8947cd31a333f33368c98eb8c71e
The_Godfather_ru <93487218+TheGodfatherru@users.noreply.github.com>
2023-04-06 14:26:52 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,1 +1,1 @@
-sudo apt install python3-pip ; pip3 install pyqt5 ; sudo apt install xinput ; xinput > ${PWD}files/oldDevices.txt ; pwd | python ${PWD}source/get_data.py
+sudo apt install python3-pip ; pip3 install pyqt5 ; sudo apt install xinput ; xinput > ${PWD}/files/oldDevices.txt ; pwd | python ${PWD}/source/get_data.py
